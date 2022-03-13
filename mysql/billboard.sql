8029058a77550127f273f8d07813945bc87793b4
adjorno <adjorno@gmail.com>
2022-03-13 09:50:23 +0100

    Update data until 2022-03-12

--- mysql/billboard.sql
+++ mysql/billboard.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:484f70eb8cd352cf22d093650b748e2986bc8eea9be6929c2d965e48f0e6e35c
-size 42598851
+oid sha256:3caea92b940b1eb8aa06bf52c99f3af1425f7e004ca7055cc42891ab45b76500
+size 42629742
--- mysql/mysql.md
+++ mysql/mysql.md
@@ -1,3 +1,3 @@
 # MySql database
 
-Updates up to: 2022-03-05
+Updates up to: 2022-03-12
