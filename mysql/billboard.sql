853519d7b2a9d3192f2255f235e7495ee0232ada
Mykhailo Dorokhin <adjorno@gmail.com>
2026-02-20 08:19:36 +0100

    fix: remove mysql files

--- mysql/billboard.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:db7c7285ece8ba77d093d57c119ef70edcd0c20e7467ceb0bed0b524ec698cba
-size 43287038
--- mysql/mysql.md
+++
@@ -1,3 +0,0 @@
-# MySql database
-
-Updates up to: 2022-08-20
